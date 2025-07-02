ba80d6b1ab787b00ee9793b77baa8fd46fa56426
romanretun <romanretunsky@gmail.com>
2025-07-03 01:33:13 +0700

    Update telegram_whisper_bot.py

--- telegram_whisper_bot.py
+++ telegram_whisper_bot.py
@@ -1,5 +1,10 @@
+import nest_asyncio
+import asyncio
+
+async def main():
+    print("Запуск бота")
+    # Здесь логика запуска бота
+
 if __name__ == '__main__':
-    import nest_asyncio
-    import asyncio
     nest_asyncio.apply()
     asyncio.run(main())
